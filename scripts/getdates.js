d61050e406be4ec308f2a740330b4319f72c2faa
xose12 <xosekarikari@gmail.com>
2024-06-18 15:00:25 -0700

    commit

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,8 +1,8 @@
 // Get the current year and display it in the element with the id 'currentyear'
 const currentYearSpan = document.getElementById('currentyear');
 const currentYear = new Date().getFullYear();
-currentYearSpan.textContent = currentYear;
+currentYearSpan.innerHTML = currentYear
 
 // Get the last modified date of the document and display it in the element with the id 'lastModified'
 const lastModifiedParagraph = document.getElementById('lastModified');
-lastModifiedParagraph.textContent = 'Last modified: ' + document.lastModified;
+lastModifiedParagraph.innerHTML = 'Last modified: ' + document.lastModified;
