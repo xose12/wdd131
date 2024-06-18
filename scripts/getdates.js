6b6668faaf6ac0955500c46b09a36c4c5ab4bd1f
xose12 <xosekarikari@gmail.com>
2024-06-18 15:27:29 -0700

    commit

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,8 +1,14 @@
-// Get the current year and display it in the element with the id 'currentyear'
-const currentYearSpan = document.getElementById('currentyear');
-const currentYear = new Date().getFullYear();
-currentYearSpan.innerHTML = currentYear
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the current year and display it in the element with the id 'currentyear'
+    const currentYearSpan = document.getElementById('currentyear');
+    if (currentYearSpan) {
+        const currentYear = new Date().getFullYear();
+        currentYearSpan.innerHTML = currentYear;
+    }
 
-// Get the last modified date of the document and display it in the element with the id 'lastModified'
-const lastModifiedParagraph = document.getElementById('lastModified');
-lastModifiedParagraph.innerHTML = 'Last modified: ' + document.lastModified;
+    // Get the last modified date of the document and display it in the element with the id 'lastModified'
+    const lastModifiedParagraph = document.getElementById('lastModified');
+    if (lastModifiedParagraph) {
+        lastModifiedParagraph.innerHTML = 'Last modified: ' + document.lastModified;
+    }
+});
